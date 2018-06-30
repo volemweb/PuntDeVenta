36f5181645e4b348570ee6cfb380554ca5e2fa2f
Salvador Rodriguez <salvasleep@hotmail.com>
2018-06-30 19:44:30 +0200

    Programa Inicial

---
+++ PuntoDeVenta/PuntoDeVenta/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace PuntoDeVenta
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTituloPrincipal = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblTituloPrincipal
+            // 
+            this.lblTituloPrincipal.AutoSize = true;
+            this.lblTituloPrincipal.Location = new System.Drawing.Point(34, 42);
+            this.lblTituloPrincipal.Name = "lblTituloPrincipal";
+            this.lblTituloPrincipal.Size = new System.Drawing.Size(135, 13);
+            this.lblTituloPrincipal.TabIndex = 0;
+            this.lblTituloPrincipal.Text = "Punt De Venta Volem Web";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(458, 353);
+            this.Controls.Add(this.lblTituloPrincipal);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTituloPrincipal;
+    }
+}
+
